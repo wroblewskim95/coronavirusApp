76f06c3d62c4a937ab6ec59b595092c6ae347d75
Mateusz Wróblewski <wroblewskim.webdeveloper@gmail.com>
2020-03-26 20:32:00 +0100

    fix from death cases to recovered cases error

--- src/js/views/worldwideView.js
+++ src/js/views/worldwideView.js
@@ -4,7 +4,7 @@ export const renderWorldwideStats = data => {
   const worldWideResult = `
   <li class="list-group-item">Confirmed:<span class="orange"> ${data.total_cases}</span><li/>
   <li class="list-group-item">Deaths:<span class="red"> ${data.total_deaths}</span><li/>
-  <li class="list-group-item">Recovered:<span class="green"> ${data.total_deaths}</span><li/>
+  <li class="list-group-item">Recovered:<span class="green"> ${data.total_recovered}</span><li/>
   <li class="list-group-item">Today Confirmed: <span class="orange">${data.new_cases}</span><li/>
   <li class="list-group-item">Today Deaths: <span class="red">${data.new_deaths}</span><li/>
   <li class="list-group-item">Last Update: ${formatDate(data.statistic_taken_at)}<li/>`;
