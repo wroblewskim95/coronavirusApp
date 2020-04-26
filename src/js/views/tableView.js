c60539c930988b07f98fb7dc71eaaef0eca74c80
Mateusz Wróblewski <wroblewskim.webdeveloper@gmail.com>
2020-04-26 17:58:19 +0200

    add deaths per million worldwide and show in table only country with name

--- src/js/views/tableView.js
+++ src/js/views/tableView.js
@@ -1,6 +1,6 @@
 import { elements } from './base';
 
-const renderCountry = country => {
+const renderCountry = (country) => {
   const tableRow = `
     <tr>
       <td>${country.country_name}</td>
@@ -13,17 +13,19 @@ const renderCountry = country => {
   return tableRow;
 };
 
-export const renderCountries = countries => {
+export const renderCountries = (countries) => {
   let listAllCountries = '';
-  countries.forEach(country => {
-    listAllCountries += renderCountry(country);
+  countries.forEach((country) => {
+    if (country.country_name) {
+      listAllCountries += renderCountry(country);
+    }
   });
   elements.tableResults.innerHTML = listAllCountries;
 };
 
-export const filterTable = input => {
+export const filterTable = (input) => {
   const allCountriesInTable = document.querySelectorAll('.all-countries tr td:first-child');
-  allCountriesInTable.forEach(country => {
+  allCountriesInTable.forEach((country) => {
     const td = country.textContent.toLocaleLowerCase();
     if (td.indexOf(input.toLocaleLowerCase()) !== -1) {
       country.parentElement.style.display = 'table-row';
--- src/js/views/worldwideView.js
+++ src/js/views/worldwideView.js
@@ -1,12 +1,15 @@
 import { elements, formatDate } from './base';
 
-export const renderWorldwideStats = data => {
+export const renderWorldwideStats = (data) => {
+  console.log(data);
+
   const worldWideResult = `
   <li class="list-group-item">Confirmed:<span class="orange"> ${data.total_cases}</span><li/>
   <li class="list-group-item">Deaths:<span class="red"> ${data.total_deaths}</span><li/>
   <li class="list-group-item">Recovered:<span class="green"> ${data.total_recovered}</span><li/>
   <li class="list-group-item">Today Confirmed: <span class="orange">${data.new_cases}</span><li/>
   <li class="list-group-item">Today Deaths: <span class="red">${data.new_deaths}</span><li/>
+  <li class="list-group-item">Deaths per 1 million people: <span class="red">${data.deaths_per_1m_population}</span><li/>
   <li class="list-group-item">Last Update: ${formatDate(data.statistic_taken_at)}<li/>`;
   elements.worldwideResults.innerHTML = worldWideResult;
 };
